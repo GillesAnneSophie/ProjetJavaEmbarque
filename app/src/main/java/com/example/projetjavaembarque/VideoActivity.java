0a118f6b4287df0864ea0d5468ea0d13fa5b2deb
Elderic <amaury.siharath@gmail.com>
2020-10-06 07:59:44 +0200

    Views setup.

--- app/src/main/java/com/example/projetjavaembarque/VideoActivity.java
+++ app/src/main/java/com/example/projetjavaembarque/VideoActivity.java
@@ -1,14 +1,14 @@
-package com.example.server;
+package com.example.projetjavaembarque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DownloadActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download);
+        setContentView(R.layout.activity_video);
     }
 }
--- server/src/main/java/com/example/server/ShareActivity.java
+++ server/src/main/java/com/example/server/ShareActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DownloadActivity extends AppCompatActivity {
+public class ShareActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download);
+        setContentView(R.layout.activity_share);
     }
 }
