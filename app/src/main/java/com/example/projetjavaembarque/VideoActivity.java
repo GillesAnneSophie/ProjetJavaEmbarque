a22d422941cf250843c08fd0518641f96a1ac91b
GillesAnneSophie <gilles.annesophiee@gmail.com>
2020-10-07 11:13:40 +0200

    Add video player and handle events

--- app/src/main/java/com/example/projetjavaembarque/VideoActivity.java
+++ app/src/main/java/com/example/projetjavaembarque/VideoActivity.java
@@ -2,49 +2,38 @@ package com.example.projetjavaembarque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-import java.io.IOException;
+public class VideoActivity extends AppCompatActivity {
+    private VideoView videoView;
+    private int currentPosition = 0;
 
-public class VideoActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-    private MediaPlayer mediaPlayer;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
-        mediaPlayer = new MediaPlayer();
-        SurfaceView surface = (SurfaceView)findViewById(R.id.surfaceView);
-        SurfaceHolder holder = surface.getHolder();
-        holder.addCallback(this);
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        holder.setFixedSize(400, 300);
+        videoView = (VideoView)findViewById(R.id.videoView);
+
+        MediaController vidControl = new MediaController(this);
+        vidControl.setAnchorView(videoView);
+        videoView.setMediaController(vidControl);
+
+        videoView.setVideoPath("https://ia800201.us.archive.org/22/items/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4");
+        videoView.start();
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        try {
-            mediaPlayer.setDisplay(holder);
-            mediaPlayer.setDataSource(Environment.getExternalStorageDirectory().getPath()+"/Movies/video.mp4");
-            //mediaPlayer.setDataSource("https://ia800201.us.archive.org/22/items/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4");
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IllegalArgumentException | IllegalStateException | IOException e) {
-            Log.d("MEDIA_PLAYER",
-                    e.getMessage());
-        }
+    public void onResume() {
+        super.onResume();
+        videoView.start();
+        videoView.seekTo(currentPosition);
     }
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) { }
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        mediaPlayer.stop();
-        mediaPlayer.release();
+    public void onPause() {
+        super.onPause();
+        currentPosition = videoView.getCurrentPosition();
+        videoView.pause();
     }
 }
